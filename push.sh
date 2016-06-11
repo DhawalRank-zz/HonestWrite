b36111bc1616f9feb255c2de7b415dc6bb1058db
Rank <sojitradhawal@gmail.com>
2016-06-11 12:36:13 -0400

    Sat, Jun 11, 2016 12:36:12 PM

--- push.sh
+++ push.sh
@@ -4,5 +4,10 @@ now=`date`
 git init
 git add .
 git commit -m "$now"
-git remote add origin https://github.com/DhawalRank/HonestWrite.git
-git push -f origin master
+if [ `git remote -v` ]; then
+	git push -f origin master
+else
+	git remote add origin https://github.com/DhawalRank/HonestWrite.git
+	git push -f origin master
+fi
+
